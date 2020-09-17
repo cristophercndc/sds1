e6b7244a4aade59b4c1145cac6459f4564f92aa5
Cristopher Costa <cristopher.cndc@gmail.com>
2020-09-17 19:07:34 -0300

    Adicionando Componentes header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/index';
 
 function App() {
   return (
-    <h1>Hello semana DevSuperior!</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
