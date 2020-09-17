48ee8cdabf3ca2113ff4bbd420394aecb75a9f03
Cristopher Costa <cristopher.cndc@gmail.com>
2020-09-17 19:55:01 -0300

    Criando pagina home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/index';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
