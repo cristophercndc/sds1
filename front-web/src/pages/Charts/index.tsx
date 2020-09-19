17f68486f54fb217f2c7829d89edf8590396c964
Cristopher Costa <cristopher.cndc@gmail.com>
2020-09-19 04:23:15 -0300

    configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -20,7 +20,7 @@ const initialPieData = {
     labels: [],
     series: []
 }
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-cristopher.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
